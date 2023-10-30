7abb71c4508e22181f0c5c9ccdb318e95d72c182
YaYa-Innovation <124864562+YaYa-Innovation@users.noreply.github.com>
2023-10-30 12:15:52 +0530

    Create Database.py

---
+++ Database.py
@@ -0,0 +1,34 @@
+# database.py
+
+from tabulate import tabulate
+import mysql.connector
+
+con = mysql.connector.connect(host="localhost", user="root", password="root", database="python_db")
+
+def insert(name, age, city):
+    res = con.cursor()
+    sql = "insert into users (name,age,city) values (%s,%s,%s)"
+    user = (name, age, city)
+    res.execute(sql, user)
+    con.commit()
+
+def update(name, age, city, id):
+    res = con.cursor()
+    sql = "update users set name=%s,age=%s,city=%s where id=%s"
+    user = (name, age, city, id)
+    res.execute(sql, user)
+    con.commit()
+
+def select():
+    res = con.cursor()
+    sql = "SELECT ID,NAME,AGE,CITY from users"
+    res.execute(sql)
+    result = res.fetchall()
+    return result
+
+def delete(id):
+    res = con.cursor()
+    sql = "delete from users where id=%s"
+    user = (id,)
+    res.execute(sql, user)
+    con.commit()
